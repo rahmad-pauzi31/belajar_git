e330ed4b8bb34ba28efabfdd88a76dfdab0d1ee0
Rahmad Pauzi <rahmadpauzi31@gmail.com>
2025-02-10 11:27:56 +0700

    commit kedua

---
+++ js/script2.js
@@ -0,0 +1,6 @@
+var ulangi = confirm("apakah anda ingin mengulangi file yang telah anda hapus");
+var counter =0;
+for (let i = 0; i < 50; i++){
+    document.write("ALLAH SWT && MUHAMMAD SAW ". repeat (50));
+}
+
